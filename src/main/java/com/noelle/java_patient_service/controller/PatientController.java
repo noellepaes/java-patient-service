b897f1ad08dcf3ca7fc7be6adaf5a4a5963459cf
noellepaes <noellepaes12345@gmail.com>
2025-05-08 14:15:24 -0300

    feat: cotroller and first structure

--- src/main/java/com/noelle/java_patient_service/controller/PatientController.java
+++ src/main/java/com/noelle/java_patient_service/controller/PatientController.java
@@ -1,12 +1,27 @@
 package com.noelle.java_patient_service.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.noelle.java_patient_service.dto.PatientResponseDto;
 import com.noelle.java_patient_service.service.PatientService;
 
 @RestController
 @RequestMapping("/api/patients")
 public class PatientController {
-    // private final PatientService patientService;
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    // @GetMapping
+    // public ResponseEntity<List<PatientResponseDto>> getPaciets() {
+    //     List<PatientResponseDto> patients = patientService.getPaciets();
+    //     return ResponseEntity.ok().body(patients);
+    // }
 }
